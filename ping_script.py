65bdd05bc40c707321d5edfdd05e552041611cc2
marymitchell44 <marymitchell1505297@gmail.com>
2024-12-17 05:57:02 +0200

    Update ping_script.py

--- ping_script.py
+++ ping_script.py
@@ -1,4 +1,12 @@
 TEST_URL = "http://httpbin.org/ip"
+PROXIES_LIST = [
+    {"http": "http://hFPncvbbuc_0:22VnEeaqTBFI@s-17704.sp6.ovh:11001",
+     "https": "https://hFPncvbbuc_0:22VnEeaqTBFI@s-17704.sp6.ovh:11001"},
+    {"http": "http://hFPncvbbuc_1:22VnEeaqTBFI@s-17704.sp6.ovh:11002",
+     "https": "https://hFPncvbbuc_1:22VnEeaqTBFI@s-17704.sp6.ovh:11002"},
+    {"http": "http://hFPncvbbuc_2:22VnEeaqTBFI@s-17704.sp6.ovh:11003",
+     "https": "https://hFPncvbbuc_2:22VnEeaqTBFI@s-17704.sp6.ovh:11003"},
+]
 
 def test_proxy():
     for proxy in PROXIES_LIST:
