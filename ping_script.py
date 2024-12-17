e50434e257dacef9ccd05d3a94dcc12032396308
marymitchell44 <marymitchell1505297@gmail.com>
2024-12-17 05:55:07 +0200

    Update ping_script.py

--- ping_script.py
+++ ping_script.py
@@ -1,57 +1,12 @@
-import requests
-import time
-from datetime import datetime
+TEST_URL = "http://httpbin.org/ip"
 
-# URL для посещения
-URL = "https://webdesign-finder.com/cogniart/404"
-
-# Заголовки для имитации браузера
-HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-    "Accept-Language": "en-US,en;q=0.5",
-    "Referer": "https://google.com",  # Добавляем Referer
-    "Accept-Encoding": "gzip, deflate, br",
-    "Connection": "keep-alive",
-    "Upgrade-Insecure-Requests": "1"
-}
-
-
-# Список прокси с логином и паролем
-PROXIES_LIST = [
-    {"http": "http://hFPncvbbuc_0:22VnEeaqTBFI@s-17704.sp6.ovh:11001",
-     "https": "http://hFPncvbbuc_0:22VnEeaqTBFI@s-17704.sp6.ovh:11001"},
-    {"http": "http://hFPncvbbuc_1:22VnEeaqTBFI@s-17704.sp6.ovh:11002",
-     "https": "http://hFPncvbbuc_1:22VnEeaqTBFI@s-17704.sp6.ovh:11002"},
-    {"http": "http://hFPncvbbuc_2:22VnEeaqTBFI@s-17704.sp6.ovh:11003",
-     "https": "http://hFPncvbbuc_2:22VnEeaqTBFI@s-17704.sp6.ovh:11003"},
-]
-
-# Функция для посещения сайта
-def visit_site():
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+def test_proxy():
     for proxy in PROXIES_LIST:
         try:
-            print(f"[{now}] Пытаюсь подключиться через прокси: {proxy['http']}")
-            response = requests.get(URL, headers=HEADERS, proxies=proxy, timeout=10)
-            if response.status_code == 200:
-                print(f"[{now}] Сайт успешно посещён через {proxy['http']}. Код ответа: {response.status_code}")
-                return
-            else:
-                print(f"[{now}] Ошибка при посещении сайта через {proxy['http']}. Код ответа: {response.status_code}")
+            print(f"Проверка прокси: {proxy['http']}")
+            response = requests.get(TEST_URL, proxies=proxy, timeout=10)
+            print(f"Ответ: {response.json()}")
         except Exception as e:
-            print(f"[{now}] Прокси {proxy['http']} не сработал. Ошибка: {e}")
-    print(f"[{now}] Все прокси из списка недоступны.")
+            print(f"Ошибка: {e}")
 
-# Планировщик
-def scheduler():
-    while True:
-        now = datetime.now()
-        if 2 <= now.hour < 5:
-            visit_site()
-            time.sleep(3)
-        else:
-            print(f"[{now}] Время вне диапазона, ожидание...")
-            time.sleep(60)
-
-if __name__ == "__main__":
-    scheduler()
+test_proxy()
