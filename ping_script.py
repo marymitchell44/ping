616f74d88999a4bc8c1b4f82dc1a42514f093f2c
marymitchell44 <marymitchell1505297@gmail.com>
2024-12-17 05:00:15 +0200

    Update ping_script.py

--- ping_script.py
+++ ping_script.py
@@ -3,9 +3,9 @@ import time
 from datetime import datetime
 
 # URL для посещения
-URL = "https://webdesign-finder.com/cogniart/404"
+URL = "http://webdesign-finder.com/cogniart/404"
 
-# Заголовки для имитации реального браузера
+# Заголовки
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
     "Accept-Language": "en-US,en;q=0.5",
@@ -15,11 +15,17 @@ HEADERS = {
     "Upgrade-Insecure-Requests": "1"
 }
 
+# Прокси (замените на свои данные)
+PROXIES = {
+    "http": "http://34.122.187.196:80",
+    "https": "http://34.122.187.196:80"
+}
+
 # Функция для посещения сайта
 def visit_site():
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Определяем время перед try
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     try:
-        response = requests.get(URL, headers=HEADERS, timeout=30)
+        response = requests.get(URL, headers=HEADERS, proxies=PROXIES, timeout=30)
         if response.status_code == 200:
             print(f"[{now}] Сайт успешно посещён. Код ответа: {response.status_code}")
         else:
@@ -27,18 +33,16 @@ def visit_site():
     except Exception as e:
         print(f"[{now}] Ошибка: {e}")
 
-# Планировщик: запуск каждую 3 секунды с 2 до 5 утра
+# Планировщик
 def scheduler():
     while True:
         now = datetime.now()
-        # Проверяем, если текущее время между 2:00 и 5:00 утра
         if 2 <= now.hour < 5:
             visit_site()
-            time.sleep(3)  # Пауза на 3 секунды
+            time.sleep(3)
         else:
-            print(f"[{now.strftime('%Y-%m-%d %H:%M:%S')}] Время вне диапазона, ожидание...")
-            time.sleep(60)  # Проверка раз в минуту
+            print(f"[{now}] Время вне диапазона, ожидание...")
+            time.sleep(60)
 
-# Запуск планировщика
 if __name__ == "__main__":
     scheduler()
