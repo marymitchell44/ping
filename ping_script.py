dc7165c6a26a003545a003c56652f22e735be139
marymitchell44 <marymitchell1505297@gmail.com>
2024-12-18 02:11:42 +0200

    Update ping_script.py

--- ping_script.py
+++ ping_script.py
@@ -55,8 +55,8 @@ if __name__ == "__main__":
         # Получаем текущее серверное время (UTC)
         current_hour = datetime.utcnow().hour
 
-        # Проверяем, что время между 1:00 и 5:00 UTC (3:00–7:00 по Киеву)
-        if 1 <= current_hour < 5:
+        # Проверяем, что время между 00:00 и 5:00 UTC (3:00–7:00 по Киеву)
+        if 0 <= current_hour < 5:
             proxy = next(proxy_pool)
             visit_site(proxy)
             time.sleep(1)  # Пауза 1 секунды между запросами
